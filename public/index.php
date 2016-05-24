ce0d69f3f6aae7da1279d268aa21c8f613b07d1b
BobbieO <bobbie2504@msn.com>
2016-05-24 09:06:10 -0500

    Added sections, z-depth to table and buttons, color to buttons, cleaned up CSS page to remove classes no longer necessary due to addition of materialize classes.

--- public/index.php
+++ public/index.php
@@ -56,15 +56,15 @@ function pageController($dbc) {
     $stmt = $dbc->prepare('SELECT COUNT(*) FROM national_parks');
     $stmt->execute();
     $count = $stmt->fetchColumn();
-    $totalPages = ceil($count/4);
+    $totalPages = ceil($count/3);
 
     $data['totalPages'] = $totalPages;
 
     $data['page'] = Input::has('page') ? Input::get('page') : 1;
 
-    $offset = ($data['page'] - 1) * 4; 
+    $offset = ($data['page'] - 1) * 3; 
 
-    $stmt = $dbc->prepare('SELECT * FROM national_parks LIMIT 4 OFFSET :offset');
+    $stmt = $dbc->prepare('SELECT * FROM national_parks LIMIT 3 OFFSET :offset');
 
     $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
     $stmt->execute();
@@ -105,97 +105,87 @@ function pageController($dbc) {
                 <h1 class="green-text text-darken-4">National Parks of the United States</h1>
             </div>  
         </div>
+        
+        <div>
+            <table class="z-depth-1">
+                <tr class="bold">
+                    <th>Name</th>
+                    <th>Location</th>
+                    <th>Date Established</th>
+                    <th>Acreage</th>
+                    <th>Description</th>
+                </tr>
+
+                <?php foreach($parks as $park) : ?>
+                    <tr><td class="bold"><?= escape($park['name']); ?></td>
+                    <td><?= escape($park['location']); ?></td>
+                    <td><?= escape($park['date_established']); ?></td>
+                    <td><?= escape($park['area_in_acres']); ?></td>
+                    <td><?= escape($park['description']); ?></td></tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
     </div>
-
-    
     <div class="section">
-        <table class="shadow">
-            <tr class="bold">
-                <th>Name</th>
-                <th>Location</th>
-                <th>Date Established</th>
-                <th>Acreage</th>
-                <th>Description</th>
-            </tr>
-
-            <?php foreach($parks as $park) : ?>
-                <tr><td class="bold"><?= escape($park['name']); ?></td>
-                <td><?= escape($park['location']); ?></td>
-                <td><?= escape($park['date_established']); ?></td>
-                <td><?= escape($park['area_in_acres']); ?></td>
-                <td><?= escape($park['description']); ?></td></tr>
+        <div class="green-text text-darken-4 center-align">
+            <?php foreach($errors as $error) : ?>
+                <p><?= $error; ?></p>
             <?php endforeach; ?>
-        </table>
-    </div>
+        </div>
 
-    <div class="section errors">
-        <?php foreach($errors as $error) : ?>
-            <p><?= $error; ?></p>
-        <?php endforeach; ?>
-    </div>
-
-    <div class="section">
-        <form method="POST">
-            <div class="form shadow">
-            <h3 class="green-text text-darken-4">Add a Park</h3>
-                <p>
-                    <span class="margin">
-                        <label for="name">Park Name</label>
-                        <input id="name" name="name" type="text" placeholder="Enter park name" REQUIRED
-                        value="<?= (isset($_POST['name'])) ? $_POST['name']: ''; ?>">
-                    </span>
+        <div>
+            <form method="POST">
+            <h3 class="green-text text-darken-4 center-align">Add a Park</h3>
+                <p> 
+                    <label for="name">Park Name</label>
+                    <input id="name" name="name" type="text" placeholder="Enter park name" REQUIRED
+                    value="<?= (isset($_POST['name'])) ? $_POST['name']: ''; ?>">
                 
-                    <span class="margin">
-                        <label for="location">Location</label>
-                        <input id="location" name="location" type="text" placeholder="Enter location" REQUIRED
-                        value="<?= (isset($_POST['location'])) ? $_POST['location']: ''; ?>">
-                    </span>
-                </p>
-                <p>
-                    <span class="margin">
-                        <label for="date_established">Date Established</label>
-                        <input id="date_established" name="date_established" type="text" placeholder="YYYY-MM-DD format" REQUIRED
-                        value="<?= (isset($_POST['date_established'])) ? $_POST['date_established']: ''; ?>">
-                    </span>
+                    <label for="location">Location</label>
+                    <input id="location" name="location" type="text" placeholder="Enter location" REQUIRED
+                    value="<?= (isset($_POST['location'])) ? $_POST['location']: ''; ?>">
+                
+                    <label for="date_established">Date Established</label>
+                    <input id="date_established" name="date_established" type="text" placeholder="YYYY-MM-DD format" REQUIRED
+                    value="<?= (isset($_POST['date_established'])) ? $_POST['date_established']: ''; ?>">
+                 
+                    <label for="area_in_acres">Acreage</label>
+                    <input id="area_in_acres" name="area_in_acres" type="text" placeholder="Numbers only, no punctuation" REQUIRED
+                    value="<?= (isset($_POST['area_in_acres'])) ? $_POST['area_in_acres']: ''; ?>">
                     
-                </p>
-                <p>
-                    <span class="margin">
-                        <label for="area_in_acres">Acreage</label>
-                        <input id="area_in_acres" name="area_in_acres" type="text" placeholder="no commas" REQUIRED
-                        value="<?= (isset($_POST['area_in_acres'])) ? $_POST['area_in_acres']: ''; ?>">
-                    </span>
-                
                     <label for="description">Description</label>
                     <textarea id="description" name="description" placeholder="Tell us about it!" REQUIRED><?= (isset($_POST['description'])) ? $_POST['description']: ''; ?></textarea>
                 </p>
                 <p>
-                    <input class="btn btn-default" type="submit" value="Send Your Park Info!">
+                    <input class="btn btn-default green darken-4 z-depth-1" type="submit" value="Add Your Park!">
                 </p>
-            </div>
-        </form>
-    </div>  
+            </form>
+        </div> 
+    </div> 
 </div>
 
-<footer class="footer"> 
-    <?php if($page > 1): ?> 
-        <a href="/index.php" class="btn btn-default">First</a>
-    <?php endif; ?>
+<div class="section">
+    <footer class="center-align"> 
+        <?php if($page > 1): ?> 
+            <a href="/index.php" class="btn btn-default green darken-4 z-depth-2">First</a>
+        <?php endif; ?>
 
-    <?php if($page >= 2): ?> 
-        <a href="?page=<?= $page - 1 ?>" class="btn btn-default">Previous</a>
-    <?php endif; ?>
+        <?php if($page >= 2): ?> 
+            <a href="?page=<?= $page - 1 ?>" class="btn btn-default green darken-4 z-depth-2">Previous</a>
+        <?php endif; ?>
 
-    <button class="btn btn-default">Page <?=$page; ?></button>
-    
-    <?php if($page < $totalPages ): ?>
-        <a href="?page=<?= $page + 1 ?>" class="btn btn-default">Next</a>
-    <?php endif; ?>
+        <button class="btn btn-default green darken-4 z-depth-2">Page <?=$page; ?></button>
+        
+        <?php if($page < $totalPages ): ?>
+            <a href="?page=<?= $page + 1 ?>" class="btn btn-default green darken-4 z-depth-2">Next</a>
+        <?php endif; ?>
 
-    <?php if($page < $totalPages ): ?>
-        <a href="?page=<?= $totalPages ?>" class="btn btn-default">Last</a>
-    <?php endif; ?>
-</footer>
+        <?php if($page < $totalPages ): ?>
+            <a href="?page=<?= $totalPages ?>" class="btn btn-default green darken-4 z-depth-2">Last</a>
+        <?php endif; ?>
+    </footer>
+</div>
+
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>        
