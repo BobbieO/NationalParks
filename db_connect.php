2f699c53aeea78cacfa45d4dd7b89cb6e2cbe43c
BobbieO <bobbie2504@msn.com>
2016-05-23 19:21:48 -0500

    Initial commit of copy of db_connect from codeup.dev

---
+++ db_connect.php
@@ -0,0 +1,9 @@
+<?php
+
+// Get new instance of PDO object
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
